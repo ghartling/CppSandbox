184226942d22d2e275aa8e94cdaaae29b411ce5b
Gerry <ghartlingjr@gmail.com>
2020-03-01 23:42:54 -0500

    added  3sandbox/.project

---
+++ 3sandbox/src/Foo.cpp
@@ -0,0 +1,18 @@
+/*
+ * Foo.cpp
+ *
+ *  Created on: Feb 19, 2020
+ *      Author: Gerald.Hartling
+ */
+
+#include "Foo.h"
+
+Foo::Foo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Foo::~Foo() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ 3sandbox/src/Foo.h
@@ -0,0 +1,18 @@
+/*
+ * Foo.h
+ *
+ *  Created on: Feb 19, 2020
+ *      Author: Gerald.Hartling
+ */
+
+#ifndef FOO_H_
+#define FOO_H_
+
+class Foo {
+public:
+	Foo();
+	virtual ~Foo();
+	bool foo() {return true;}
+};
+
+#endif /* FOO_H_ */
